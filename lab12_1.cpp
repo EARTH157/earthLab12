305a75468769e3e2e14f684cd3429a95c6d3034f
EARTH157 <earthzz.jirapat@outlook.com>
2023-01-06 15:58:51 +0700

    Finish

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,17 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<string>
+using namespace std;
+
+int main(){
+    srand(time(0));
+    string grade[9] = {"A","B+","B","C+","C","D+","D","F","W"};
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    int roll = rand()%9;
+    cout << "You will get " << grade[roll]<< " in this 261102.";
+    return 0;
+}
